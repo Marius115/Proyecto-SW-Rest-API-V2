8ded346e765e22252e50f9a72baecc5825ee4518
Marius115 <cascaranomeister@gmail.com>
2022-07-11 15:25:25 +0000

    :goberserk: endpoints agregados.

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User, Character, Planet, Favorite
+from models import db, User,Character,Planet,Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,107 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+#Endpoint para el o los usuarios
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def user_list():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+	user = User.query.all()
+	all_users = list(map(
+        lambda user: user.serialize(),user))
+        
+	return jsonify(all_users), 200
 
-    return jsonify(response_body), 200
+# Endpoints de todos los characters y del character individual
+
+@app.route('/character', methods=['GET'])
+def character_list():
+   
+    character = Character.query.all()
+    all_characters = list(map(
+        lambda character: character.serialize(),character)) 
+    
+    return jsonify(all_characters), 200
+    
+
+@app.route('/character/<int:character_id>', methods=['PUT', 'GET'])
+def get_single_character(character_id):
+    body = request.get_json() 
+    if request.method == 'PUT':
+        single_character = Character.query.get(character_id)
+        single_character.name = body.name
+        db.session.commit()
+    if request.method == 'GET':
+        single_character = Character.query.get(character_id)
+        return jsonify(single_character.serialize()), 200
+
+
+ # Endpoints de todos los planets y de planet individual
+@app.route('/planet', methods=['GET'])
+def planet_list():
+   
+    planet = Planet.query.all()
+    all_planets = list(map(
+        lambda planet: planet.serialize(),planet)) 
+    
+    return jsonify(all_planets), 200
+
+@app.route('/planet/<int:planet_id>', methods=['PUT', 'GET'])
+def get_single_planet(planet_id):
+    body = request.get_json() #{ 'username': 'new_username'}
+    if request.method == 'PUT':
+        single_planet = Planet.query.get(planet_id)
+        single_planet.name = body.name
+        db.session.commit()
+    if request.method == 'GET':
+        single_planet = Planet.query.get(planet_id)
+        return jsonify(single_planet.serialize()), 200
+
+#Endpoint de favorites:
+@app.route('/user/favorite', methods=['GET'])
+def handle_favorite():
+
+    favorite = Favorite.query.all()
+    all_favorites = list(map(
+        lambda favorite: favorite.serialize(),favorite))
+    return jsonify(all_favorites), 200
+
+
+@app.route('/favorite/planet/<int:planet_id>',methods=['POST'])
+def add_planet_favorite(planet_id):
+
+	body = request.get_json()
+	new_favorite = Favorite(
+		user_id=body["user_id"],
+		planet_id=body["planet_id"],
+		)
+	db.session.add(new_favorite)
+	db.session.commit()
+	return jsonify(new_favorite.serialize()), 200
+
+
+@app.route('/favorite/character/<int:character_id>',methods=['POST'])
+def add_character_favorite(character_id):
+
+	body = request.get_json()
+	new_favorite = Favorite(
+		user_id=body["user_id"],
+		character_id=body["character_id"],
+		)
+	db.session.add(new_favorite)
+	db.session.commit()
+	return jsonify(new_favorite.serialize()), 200
+
+
+@app.route('/user/<int:user_id>/favorite', methods=['GET'])
+def handle_user_fav(user_id):
+
+    favorites = Favorite.query.filter_by(user_id=user_id)
+    return jsonify([fav.serialize() for fav in favorites]), 200
+
+
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
